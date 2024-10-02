de9f76b0451be3cb12a4390ecb60fa66a454a11b
Matthieu Peeters <m.peeters@iknl.nl>
2024-10-02 11:06:30 +0200

    Fix easydraw issue.

--- thirdparty/thirdparty-init.el
+++ thirdparty/thirdparty-init.el
@@ -1,17 +1,21 @@
+(when nil 
 
 
-; Easydraw
-; [[https://github.com/misohena/el-easydraw][el-easydraw]]
+                                        ; Easydraw
+                                        ; [[https://github.com/misohena/el-easydraw][el-easydraw]]
 
-(add-to-list 'load-path (concat (file-name-directory (or load-file-name (buffer-file-name))) "./el-easydraw/"))
+  (add-to-list 'load-path (concat (file-name-directory (or load-file-name (buffer-file-name))) "./el-easydraw/"))
 
-(with-eval-after-load 'org
-  (require 'edraw-org)
-  (edraw-org-setup-default))
-;; When using the org-export-in-background option (when using the
-;; asynchronous export function), the following settings are
-;; required. This is because Emacs started in a separate process does
-;; not load org.el but only ox.el.
-(with-eval-after-load "ox"
-  (require 'edraw-org)
-  (edraw-org-setup-exporter))
+
+
+  (with-eval-after-load 'org
+    (require 'edraw-org)
+    (edraw-org-setup-default))
+  ;; When using the org-export-in-background option (when using the
+  ;; asynchronous export function), the following settings are
+  ;; required. This is because Emacs started in a separate process does
+  ;; not load org.el but only ox.el.
+
+  (with-eval-after-load "ox"
+    (require 'edraw-org)
+    (edraw-org-setup-exporter)))
